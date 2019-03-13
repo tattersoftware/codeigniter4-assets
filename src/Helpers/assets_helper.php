748c85366746813f71bbf2e56362de932b44b6d7
MGatner <mgatner@icloud.com>
2019-03-13 16:32:04 -0400

    tweak namespaces for automagic CI4 calls

--- src/Helpers/assets_helper.php
+++ src/Helpers/assets_helper.php
@@ -4,9 +4,8 @@
 *
 * This file contains optional helper functions to make calling the library easier.
 * Recommended usage:
-*	1. Copy the file to app/Helpers/assets_helper.php
-*	2. Load the helper with `helper("assets")`
-*	3. Call `css()` in <head> and `js()` before </body>
+*	1. Load the helper with `helper("assets")`
+*	2. Call `css()` in <head> and `js()` before </body>
 *
 ***/
 
@@ -15,7 +14,7 @@ if (! function_exists('css'))
 	// outputs all route-relevant and configured CSS tags, or
 	// given a path outputs a single CSS tag
 	function css(string $file = null) {
-		$assets = new Tatter\Assets();
+		$assets = new Tatter\Libraries\Assets();
 	
 		// intercept requests for a single file
 		if (is_string($file))
@@ -26,11 +25,11 @@ if (! function_exists('css'))
 }
 
 if (! function_exists('js'))
-	{
+{
 	// outputs all route-relevant and configured JS tags, or
 	// given a path outputs a single JS tag
 	function js(string $file = null) {
-		$assets = new Tatter\Assets();
+		$assets = new Tatter\Libraries\Assets();
 	
 		// intercept requests for a single file
 		if (is_string($file))
