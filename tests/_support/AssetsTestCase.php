fd80be89a6ed0ba563bfbf4dce31b290fcc5a5bb
MGatner <mgatner@icloud.com>
2020-07-11 17:50:40 +0000

    Test action

--- tests/_support/AssetsTestCase.php
+++ tests/_support/AssetsTestCase.php
@@ -9,20 +9,20 @@ class AssetsTestCase extends CIUnitTestCase
 	 * @var \Tatter\Assets\Libraries\Assets
 	 */
 	protected $assets;
-	
+
 	/**
 	 * @var \Tatter\Assets\Config\Assets
 	 */
 	protected $config;
-	
+
     public function setUp(): void
     {
         parent::setUp();
-		
+
 		$this->config           = new \Tatter\Assets\Config\Assets();
 		$this->config->silent   = false;
 		$this->config->fileBase = SUPPORTPATH . 'assets/';
-		
+
 		// Create the service
 		$this->assets = new Assets($this->config);
 	}
